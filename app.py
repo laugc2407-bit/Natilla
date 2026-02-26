99651e758dddf8c30f15801600731126b8a1fcf5
laugc2407-bit <laugc2407@gmail.com>
2026-02-26 10:14:07 -0500

    app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from PIL import image
+from PIL import Image
 st.title ("Que emoción, mi primera app :D")
 st.header ("En este espacio comienzo a desarrollar mis apps para interfases")
 st.write ("Facilmente puedo realizar backend y frontend")
