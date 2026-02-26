3b741a95367c217b6082504d13d1e97e5fe8c03b
laugc2407-bit <laugc2407@gmail.com>
2026-02-26 10:48:35 -0500

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ st.write ("mira este gatote bien feliz y coqueton")
 image = Image.open ('gaton.jpg')
 st.image (image, caption = 'que felicidad')
 
-texto = st.text_input ('escribe algo, no sé como que ojalá ser ese gato' , 'gatote feliz y coqueton')
+texto = st.text_input ('escribe algo, no sé como que ojalá ser ese gato' , 'tu textote')
 st.write ('el texto escrito es', texto)
 
 st.subheader ("Ahora usemos dos columnas")
