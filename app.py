4bc7a195b65c0c9d2de190edd75c43730c12ec93
laugc2407-bit <laugc2407@gmail.com>
2026-02-26 10:17:07 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 import streamlit as st
 from PIL import Image
+
 st.title ("Que emoción, mi primera app :D")
 st.header ("En este espacio comienzo a desarrollar mis apps para interfases")
 st.write ("Facilmente puedo realizar backend y frontend")
 image = Image.open ('gaton.jpg')
 st.image (image, caption = 'que felicidad')
 
+texto = st.text_input ('escribiendo algo, no sé que escribir, ojalá ser ese gato' , 'gatote feliz y coqueton')
+st.write ('el texto escritoes', texto)
