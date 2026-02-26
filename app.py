5fbc99e6496c4a69b274c38b51a45e3d7de137cd
laugc2407-bit <laugc2407@gmail.com>
2026-02-26 10:12:56 -0500

    app.py

--- app.py
+++ app.py
@@ -3,5 +3,5 @@ st.title ("Que emoción, mi primera app :D")
 st.header ("En este espacio comienzo a desarrollar mis apps para interfases")
 st.write ("Facilmente puedo realizar backend y frontend")
 image = Image.open ('gaton.jpg')
-st.image (imagen, caption = 'que felicidad')
+st.image (image, caption = 'que felicidad')
 
